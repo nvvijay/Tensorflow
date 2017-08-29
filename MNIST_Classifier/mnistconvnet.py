e1c29b28bd203ffc25bc207436193e5820b2b169
Vijay N V <vijay-2391@vijay-2391.csez.zohocorpin.com>
2017-08-29 13:31:01 +0530

    added save and restore

--- MNIST_Classifier/mnistconvnet.py
+++ MNIST_Classifier/mnistconvnet.py
@@ -1,3 +1,4 @@
+import os
 import tensorflow as tf
 
 from tensorflow.examples.tutorials.mnist import input_data
@@ -52,15 +53,22 @@ cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 for i in range(20000):
-  batch = mnist.train.next_batch(5)
+  batch = mnist.train.next_batch(50)
   if i%100 == 0:
     train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
     print("step %d, training accuracy %g"%(i, train_accuracy))
   train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 print("test accuracy %g"%accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+#save model after train complete
+path = saver.save(
+  sess, './saved_models/convnet_save.ckpt',
+  write_meta_graph=False, write_state=False)
+print("Saved:", path)
 
 sess.close()
